a5e71c31b130ea52b9d1897bf45ba427794b1a1f
blackjackyau <yau.yik.shiung@gmail.com>
2021-08-07 21:28:01 +0800

    upgrade diagram for better illustration, + Diagnostics

--- src/main/java/com/blax/dist/datastore/hz/HazelcastController.java
+++ src/main/java/com/blax/dist/datastore/hz/HazelcastController.java
@@ -1,14 +1,13 @@
 package com.blax.dist.datastore.hz;
 
+import com.hazelcast.collection.IList;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.Map;
 
 @RestController
@@ -19,14 +18,21 @@ public class HazelcastController {
 
     private HazelcastInstance hazelcastInstance;
 
-    @RequestMapping(value = "values", method = RequestMethod.GET)
-    Map getValues() {
-        return hazelcastInstance.getMap("values");
+    @RequestMapping(value = "map/{key}", method = RequestMethod.GET)
+    Map getMap(@PathVariable String key) {
+        return hazelcastInstance.getMap(key);
     }
 
-    @RequestMapping(value = "values", method = RequestMethod.POST)
-    Map setValues(@RequestBody Map values) {
-        IMap<Object, Object> map = hazelcastInstance.getMap("values");
+    @RequestMapping(value = "list/{key}", method = RequestMethod.GET)
+    Collection getList(@PathVariable String key) {
+        return hazelcastInstance.getList(key + "List");
+    }
+
+    @RequestMapping(value = "map/{key}", method = RequestMethod.POST)
+    Map setMap(@PathVariable String key, @RequestBody Map values) {
+        IMap<Object, Object> map = hazelcastInstance.getMap(key);
+        IList<Object> keyList = hazelcastInstance.getList(key + "List");
+        keyList.addAll(values.keySet());
         map.setAll(values);
         return map;
     }
